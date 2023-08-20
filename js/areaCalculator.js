6bee6090e5ec9315ec13163eb6a4fe739c8d12a6
mashuk-tamim <tamimmashuk328@gmail.com>
2023-08-20 13:54:07 +0600

    area calculator site done

--- js/areaCalculator.js
+++ js/areaCalculator.js
@@ -1,19 +1,45 @@
 //triangle area calculator function
 function calculateTriangleArea (){
-    const baseValue = getInputValueByID('base-value');
-    const heightValue = getInputValueByID('height-value');
+    const baseValue = getInputValueByID('triangle-base');
+    const heightValue = getInputValueByID('triangle-height');
+    // console.log(baseValue, heightValue, typeof baseValue, typeof heightValue);
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('Please input a number');
+        return;
+    }
     const triangleArea = 0.5*baseValue*heightValue;
     // console.log(triangleArea);
     
     updateCalculatedArea('triangle-area', triangleArea);
+    updateAreaEntry('Triangle', triangleArea);
 }
 
 //rectangle area calculator function
 function calculateRectangleArea(){
-    const widthValue = getInputValueByID('width-value');
-    const lengthValue = getInputValueByID('length-value');
+    const widthValue = getInputValueByID('rectangle-width');
+    const lengthValue = getInputValueByID('rectangle-length');
+    if(isNaN(widthValue) || isNaN(lengthValue)){
+        alert('Please input a number');
+        return;
+    }
     const rectangleArea = widthValue*lengthValue;
     // console.log(triangleArea);
     
     updateCalculatedArea('rectangle-area', rectangleArea);
+    updateAreaEntry('Rectangle', rectangleArea);
+}
+
+//parallelogram area calculator function
+function calculateParallelogramArea(){
+    const baseValue = getInputValueByID('parallelogram-base');
+    const heightValue = getInputValueByID('parallelogram-height');
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('Please input a number');
+        return;
+    }
+    const parallelogramArea = baseValue * heightValue;
+    // console.log(triangleArea);
+    
+    updateCalculatedArea('parallelogram-area', parallelogramArea);
+    updateAreaEntry('Parallelogram', parallelogramArea);
 }
--- js/utilities.js
+++ js/utilities.js
@@ -13,3 +13,13 @@ function updateCalculatedArea(areaID, areaValue){
     getArea.innerText = areaValue;
     // console.log(getArea.innerText);
 }
+
+//Area entry function
+function updateAreaEntry(areaName, areaValue){
+    const areaEntry = document.getElementById('area-entry');
+
+    const count = areaEntry.childElementCount;
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = `${count+1}. ${areaName}: ${areaValue} cm<sup>2</sup> <button class="btn btn-xs btn-success ml-2"><p style="text-transform: lowercase">convert m<sup>2</sup></p>`;
+    areaEntry.appendChild(paragraph);
+}
